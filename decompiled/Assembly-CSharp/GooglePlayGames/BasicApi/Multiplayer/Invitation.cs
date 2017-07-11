26c522e29c33a3cfd4c5ce08ef8a8c8928c3a244
Andrew Pong <a@awp.io>
2017-07-11 16:03:52 -0700

    removed useless dotpeak header info from prior source code

--- decompiled/Assembly-CSharp/GooglePlayGames/BasicApi/Multiplayer/Invitation.cs
+++ decompiled/Assembly-CSharp/GooglePlayGames/BasicApi/Multiplayer/Invitation.cs
@@ -1,8 +1,6 @@
 ﻿// Decompiled with JetBrains decompiler
 // Type: GooglePlayGames.BasicApi.Multiplayer.Invitation
 // Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 2EE8B15F-8D58-4BD6-8905-91665367FCCE
-// Assembly location: C:\Users\Andrew\Downloads\base\assets\bin\Data\Managed\Assembly-CSharp.dll
 
 namespace GooglePlayGames.BasicApi.Multiplayer
 {
