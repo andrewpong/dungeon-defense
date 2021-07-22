b5632498246f82c63c22b93a81862a6ea41b349a
Andrew Pong <a@awp.io>
2021-07-21 20:17:53 -0700

    Removing 1.93.2 stuff to reduce noise

--- decompiled/Assembly-CSharp/1.93.2/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Configuration.Assemblies;
-using System.Reflection;
-
-[assembly: AssemblyAlgorithmId(AssemblyHashAlgorithm.None)]
-[assembly: AssemblyVersion("3.7.1.6")]
